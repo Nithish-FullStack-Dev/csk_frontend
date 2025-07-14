23a5ea73c4b897993b30bc21e917a72bc414e3ef
AnilKumt <anilkumt51@gmail.com>
2025-07-14 11:31:42 +0530

    made firebase configuration safe

--- src/config/firebaseConfig.ts
+++ src/config/firebaseConfig.ts
@@ -3,14 +3,14 @@ import { getDatabase } from "firebase/database";
 import {getAuth} from "firebase/auth"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyATJBUYfxh8OI6_rjFj0NR1raQnKfaGhGo",
-  authDomain: "react-firebase-practice-e1407.firebaseapp.com",
-  databaseURL: "https://react-firebase-practice-e1407-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "react-firebase-practice-e1407",
-  storageBucket: "react-firebase-practice-e1407.firebasestorage.app",
-  messagingSenderId: "1045958114919",
-  appId: "1:1045958114919:web:197085ce45043edad51ec2",
-  measurementId: "G-KJ7SNM2RTK"
+  apiKey: import.meta.env.FIREBASE_API_KEY,
+  authDomain: import.meta.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.FIREBASE_DATABASE_URL,
+  projectId: import.meta.env.FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.FIREBASE_APP_ID,
+  measurementId: import.meta.env.FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
